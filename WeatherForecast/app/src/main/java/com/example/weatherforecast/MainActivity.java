51c917dbd6188055ef8cbc82ce05a2e40fd5ec28
ETK486 <keerthivaasankarthi@gmail.com>
2024-04-03 15:31:19 +0530

    Event handling and addition another page

---
+++ WeatherForecast/app/src/main/java/com/example/weatherforecast/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.weatherforecast;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button search;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        search = findViewById(R.id.button);
+
+        search.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                if (action == MotionEvent.ACTION_DOWN) {
+                    startActivity(new Intent(MainActivity.this, MainActivity2.class));
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+}
